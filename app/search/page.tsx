1155f157140c3c783c8bb195402b70dc85ac9dbe
Arunkumar200103 <arunkumar46853@gmail.com>
2026-03-12 10:15:50 +0530

    updated ui

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = "force-dynamic"
+
 import { useSearchParams } from 'next/navigation'
 import { Navbar } from '@/components/navbar'
 import { Footer } from '@/components/footer'
